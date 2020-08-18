7d708a4aa70ca6bdf0f48401e2e8015f8640635e
Jarrett <khunicycler@hotmail.com>
2020-08-18 12:26:54 -0600

    forms: fix date/time validation
    In order for the DateTimeField to properly validate, you need to tell
    it what formats are allowed for input. We are formatting a specific
    way now with the Bootstrap date/time picker, so match that format in
    the form so it doesn't think it's invalid.

--- profit/forms.py
+++ profit/forms.py
@@ -1,30 +1,30 @@
-from django.forms import ModelForm
+from datetime import datetime
+from django.forms import ModelForm, DateTimeField
 from tempus_dominus.widgets import DateTimePicker
 from .models import Job
 
 
 class AddJobForm(ModelForm):
+    clock_in = DateTimeField(
+        input_formats=['%m/%d/%Y %I:%M %p'],
+        widget=DateTimePicker(
+            attrs={'autocomplete': 'off'},
+            options={'format': 'MM/DD/YYYY hh:mm A', 'viewMode': 'times'}
+        )
+    )
+    clock_out = DateTimeField(
+        input_formats=['%m/%d/%Y %I:%M %p'],
+        widget=DateTimePicker(
+            attrs={'autocomplete': 'off'},
+            options={'format': 'MM/DD/YYYY hh:mm A', 'viewMode': 'times'}
+        )
+    )
+
     class Meta:
         model = Job
         fields = [
             'name', 'revenue', 'job_type', 'employee', 'clock_in', 'clock_out',
         ]
-        widgets = {
-            'clock_in': DateTimePicker(
-                attrs={'autocomplete': 'off'},
-                options={
-                    'format': 'MM/DD/YYYY hh:mm A',
-                    'viewMode': 'times',
-                },
-            ),
-            'clock_out': DateTimePicker(
-                attrs={'autocomplete': 'off'},
-                options={
-                    'format': 'MM/DD/YYYY hh:mm A',
-                    'viewMode': 'times',
-                },
-            ),
-        }
 
     def __init__(self, *args, **kwargs):
         super(AddJobForm, self).__init__(*args, **kwargs)
