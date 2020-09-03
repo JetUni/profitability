5195b9c8030ab34dfe3788a5f88e0a28365ebed0
Jarrett <khunicycler@hotmail.com>
2020-09-03 17:57:09 -0600

    refactor(profit): add csrf before ajax requests
    This creates a main.js file and sets the csrf token before all the
    ajax requests that need it

---
+++ static/js/main.js
@@ -0,0 +1,31 @@
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+
+      const parts = cookie.split('=');
+      if (parts[0] === name) {
+        cookieValue = decodeURIComponent(parts[1]);
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+
+function csrfSafeMethod(method) {
+  // these HTTP methods do not require CSRF protection
+  return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+var csrftoken = getCookie('csrftoken');
+
+$.ajaxSetup({
+  beforeSend: function(xhr, settings) {
+    if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+      xhr.setRequestHeader("X-CSRFToken", csrftoken);
+    }
+  }
+});
